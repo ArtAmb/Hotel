1ebf89996362aa671de4a65bd86319602d20c600
Karolina <karolinabatkowska@op.pl>
2018-04-22 17:17:58 +0200

    Próba naprawy

---
+++ paihotel/src/main/java/hotel/dao/HotelDAO.java
@@ -0,0 +1,15 @@
+package hotel.dao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
+import hotel.domain.Hotel;
+
+@Named
+@Stateless
+@LocalBean
+public class HotelDAO extends CrudDAO<Integer, Hotel> {
+	
+	
+}
