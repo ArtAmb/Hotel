f8d17270dc8660fb8566a61e0201e19020c02c5a
Karolina <karolinabatkowska@op.pl>
2018-06-26 19:49:17 +0200

    Zakładki Zadan (wszystkie,do zrobienia, w trakcie, zakończone), zmiany statusów, zmiany w bazie danych

--- paihotel/src/main/java/hotel/domain/KeyCard.java
+++ paihotel/src/main/java/hotel/domain/KeyCard.java
@@ -1,6 +1,8 @@
 package hotel.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,7 +20,8 @@ public class KeyCard implements Domain {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	private boolean active;
+	@Enumerated(EnumType.STRING)
+	private CardStatus state;
 	
 	private String comments;
 	private Integer codeNumber;
